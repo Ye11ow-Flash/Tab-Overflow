4b7382f021aae52db45312d03525ae32f891f696
Jaineel Shah <jaineel.ns@somaiya.edu>
2020-05-10 15:23:07 +0530

    Update FirebaseHandler.js

--- Tab-Overflow/Firebasehandler/FirebaseHandler.js
+++ Tab-Overflow/Firebasehandler/FirebaseHandler.js
@@ -14,14 +14,14 @@ database = initialize();
 function initialize() {
 	// Initialize Firebase
 	var config = {
-		apiKey: "AIzaSyBZLjAibU87dTVm4-9LwEXSP3lrN1fO31M",
-	    authDomain: "tab-manager-6664d.firebaseapp.com",
-	    databaseURL: "https://tab-manager-6664d.firebaseio.com",
-	    projectId: "tab-manager-6664d",
-	    storageBucket: "tab-manager-6664d.appspot.com",
-	    messagingSenderId: "860645458052",
-	    appId: "1:860645458052:web:32f8786cb8b1c2b48402d1",
-	    measurementId: "G-WPHWP33MWQ"
+		apiKey: "",
+	    authDomain: "",
+	    databaseURL: "",
+	    projectId: "",
+	    storageBucket: "",
+	    messagingSenderId: "",
+	    appId: "",
+	    measurementId: ""
 	};
 	firebase.initializeApp(config);
   
